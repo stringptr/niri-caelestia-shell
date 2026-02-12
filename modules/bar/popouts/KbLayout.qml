26heij/caelestia-dots-kde

popouts/kblayout: add switch layout button (#682)
* keyboard layout

* Update KbLayout.qml

Code cleanup

* Update KbLayout.qml

more code cleanup

* fix

---------

Co-authored-by: 2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>

modules/bar/popouts/KbLayout.qml
@@ -1,6 +1,28 @@
 import qs.components
+import qs.components.controls
 import qs.services
+import qs.config
+import Quickshell
+import QtQuick.Layouts
 
-StyledText {
-    text: qsTr("Keyboard layout: %1").arg(Hypr.kbLayoutFull)
+ColumnLayout {
+    id: root
+
+    spacing: Appearance.spacing.normal
+
+    StyledText {
+        Layout.topMargin: Appearance.padding.normal
+        Layout.rightMargin: Appearance.padding.normal
+        text: qsTr("Keyboard layout: %1").arg(Hypr.kbLayoutFull)
+        font.weight: 500
+    }
+
+    TextButton {
+        Layout.bottomMargin: Appearance.padding.normal
+        Layout.rightMargin: Appearance.padding.normal
+        Layout.fillWidth: true
+
+        text: qsTr("Switch layout")
+        onClicked: Quickshell.execDetached(["hyprctl", "switchxkblayout", "all", "next"])
+    }
 }

stringptr/niri-caelestia-shell

modules/bar/popouts/KbLayout.qml
@@ -1,6 +1,29 @@
 import qs.components
+import qs.components.controls
 import qs.services
+import qs.config
+import Quickshell
+import QtQuick.Layouts
 
-StyledText {
-    text: qsTr("Keyboard layout: %1").arg(Niri.kbLayouts)
+ColumnLayout {
+    id: root
+
+    spacing: Appearance.spacing.normal
+
+    StyledText {
+        Layout.topMargin: Appearance.padding.normal
+        Layout.rightMargin: Appearance.padding.normal
+        text: qsTr("Keyboard layout: %1").arg(Niri.kbLayouts)
+        // text: qsTr("Keyboard layout: %1").arg(Hypr.kbLayoutFull)
+        font.weight: 500
+    }
+
+    // TextButton {
+    //     Layout.bottomMargin: Appearance.padding.normal
+    //     Layout.rightMargin: Appearance.padding.normal
+    //     Layout.fillWidth: true
+    //
+    //     text: qsTr("Switch layout")
+    //     onClicked: Quickshell.execDetached(["hyprctl", "switchxkblayout", "all", "next"])
+    // }
 }