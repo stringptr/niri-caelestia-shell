3418ad80741a5a3a671ee25e27cf185af4350ff0
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2026-03-19 23:00:23 +1100

    fix: sparkline div by 0

--- plugin/src/Caelestia/Internal/sparklineitem.cpp
+++ plugin/src/Caelestia/Internal/sparklineitem.cpp
@@ -27,6 +27,9 @@ void SparklineItem::paint(QPainter* painter) {
 }
 
 void SparklineItem::drawLine(QPainter* painter, CircularBuffer* buffer, const QColor& color, qreal fillAlpha) {
+    if (m_historyLength < 2)
+        return;
+
     const qreal w = width();
     const qreal h = height();
     const int len = buffer->count();
